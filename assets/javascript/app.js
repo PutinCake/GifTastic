e03a12e462a198e2dcbb31cea56cbe33b56c82ab
PutinCake <zhiyemo09@gmail.com>
2018-05-06 19:17:37 -0700

    fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,11 +26,8 @@ $(document).ready(function(){
                     animalImage.attr('data-animate', results[i].images.fixed_height.url)
 
                     .attr('data-state', 'still');
-
                     animalDiv.append(p);
-
                     animalDiv.append(animalImage);
-
                     animalDiv.prependTo($('#gifs'));
                 }
 
@@ -40,97 +37,71 @@ $(document).ready(function(){
                     console.log(this);
 
                     if (state == 'still') {
-                    
                     $(this).attr('src', $(this).data('animate'));
-                    
                     $(this).attr('data-state', 'animate');
-
-                    } else {
-                            
+                    } else { 
                     $(this).attr('src', $(this).data('still'));
-                    
                     $(this).attr('data-state', 'still');
                     }      
                 });
             });
     });
-
+    //add the search terms to buttons section
     var animals = [''];
 
-    
-        //This function "adds" the buttons 
+    //input section button
+    $('#theButton').on('click', function(){
+         //set search word
+        var animalButton = $("#gif-input").val();
+        var newButton = $("<button/>").addClass( "btn btn-info animal").attr('data-name',animalButton).html(animalButton).css({'margin': '5px'});
+        
+        $("#animalsbuttons").append(newButton);
+        console.log("Work");
 
-        // handles the event when clicked
-        $('#theButton').on('click', function(){
-            var animalButton = $("#gif-input").val();
-            //adds the new animal
+        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalButton + "&api_key=dc6zaTOxFJmzC&limit=10";
+        console.log(animalButton);
 
-            var newButton = $("<button/>").addClass( "btn btn-info animal").attr('data-name',animalButton).html(animalButton).css({'margin': '5px'});
-            
-            $("#animalsbuttons").append(newButton);
-                console.log("Work");
-
-            queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalButton + "&api_key=dc6zaTOxFJmzC&limit=10";
-                console.log(animalButton);
-
-            $.ajax({
+        $.ajax({
             url: queryURL,
             method: 'GET'
-            })
-
+        })
             .done(function(response) {
 
             var results = response.data;
 
                 for (var i = 0; i < results.length; i++) {
-
                     var animalDiv = $('<div/>');
-
                     var p =$('<p/>');
-
-                    p.text(results[i].rating);
+                    p.text("Rating: " + results[i].rating);
 
                     var animalImage = $('<img/>');
-
                     animalImage.addClass('anImg')
-
                     animalImage.attr('src', results[i].images.fixed_height_still.url);
-
                     animalImage.attr('data-still', results[i].images.fixed_height_still.url)
-
                     animalImage.attr('data-animate', results[i].images.fixed_height.url)
 
                     .attr('data-state', 'still');
-
                     animalDiv.append(p);
-
                     animalDiv.append(animalImage);
-
                     animalDiv.prependTo($('#gifs'));
                 }
 
                 $('.anImg').on('click', function() {
-            
                     var state = $(this).attr('data-state'); 
                     console.log(this);
 
-                    if (state == 'still') {
-                    
+                    if (state == 'still') {              
                     $(this).attr('src', $(this).data('animate'));
-                    
                     $(this).attr('data-state', 'animate');
-
-                    } else {
-                            
-                    $(this).attr('src', $(this).data('still'));
-                    
+                    } else {            
+                    $(this).attr('src', $(this).data('still')); 
                     $(this).attr('data-state', 'still');
                     }      
                 });
             });
 
-            $("#gif-input").val("");
-            return false;
-        })
+        $("#gif-input").val("");
+        return false;
+    })
   
 });
